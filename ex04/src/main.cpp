ee5e2bb83eca45afa30f1330f90b97b4de69bc6f
Alisa Valiullina <avaliull@f1r2s13.codam.nl>
2026-02-06 17:03:43 +0100

    editting file editting file

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -11,8 +11,38 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
-int	main(void) {
-	std::cout << "hi\n";
+int	main(
+	int		argc,
+	[[maybe_unused]] char*	argv[]
+) {
+	if (argc != 4) {
+		std::cout << "Program needs 3 arguments: filename, string1 and string2\n";
+		return (1);
+	}
+
+	std::fstream	target_file(argv[1]);
+	std::string		old_str = argv[2];
+	std::string		new_str = argv[3];
+
+	std::string	file_contents;
+	std::string	next_line;
+
+	while (std::getline(target_file, next_line)) {
+		file_contents.append(next_line);
+		file_contents.append("\n");
+	}
+	std::cout << "File contents before:\n" << file_contents;
+	size_t	found_instance = file_contents.find(old_str);
+	while (found_instance != std::string::npos) {
+		file_contents.erase(found_instance, old_str.length());
+		file_contents.insert(found_instance, new_str);
+		found_instance = file_contents.find(old_str, found_instance + old_str.length());
+	}
+	std::cout << "File contents after:\n" << file_contents;
+	target_file.write(file_contents, file_contents.length());
+
+	target_file.close();
 	return 0;
 }
