b801fcc813741e44e444d67775ed5c6ed7ec3d18
Alisa Valiullina <avaliull@f0r2s3.codam.nl>
2026-03-13 20:27:10 +0100

    overthinking ex04 tests

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: avaliull <avaliull@student.codam.nl>              +#+                */
 /*                                                        +#+                 */
 /*   Created: 2026/02/03 17:31:30 by avaliull            #+#    #+#           */
-/*   Updated: 2026/03/13 19:57:13 by avaliull            ########   odam.nl   */
+/*   Updated: 2026/03/13 20:26:21 by avaliull            ########   odam.nl   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,8 @@ using std::cout;
 
 // tests
 #include <iomanip>
+#include <fstream>
+#include <iostream>
 using std::setw;
 
 static const string	clr_non = "\033[0m";
@@ -53,15 +55,17 @@ static const string	clr_red = "\033[31m";
 static const string	clr_grn = "\033[92m";
 static const string	clr_mag = "\033[95m";
 
-void	printReturnValue(
-	int ret_val
+void	print_file_contents(
+	string			filename
 ) {
-	string	color;
-	if (ret_val == 0)
-		color = clr_grn;
-	else if (ret_val == 1)
-		color = clr_red;
-	cout << clr_yel << "RETURN VALUE:" << clr_cya << ret_val << "\033[0m\n";
+	std::ifstream	file(filename);
+
+	cout << clr_yel << filename << ": " << clr_non;
+	if (file.is_open() == true)
+		cout << file.rdbuf() << '\n';
+	else
+		cout << clr_cya << "can't be opened/does not exist" << clr_non << '\n';
+	file.close();
 }
 
 int	main(
@@ -69,12 +73,14 @@ int	main(
 ) {
 	int				ret_val;
 	string			filename;
+	string			filename_r;
 	string			str1;
 	string			str2;
 
 	cout << '\n';
 	// TEST 1: file that doesn't exist
 	filename = "testfiles/fakefile";
+	filename_r = "testfiles/fakefile.replace";
 	str1 = "meow";
 	str2 = "bark";
 	cout << clr_yel << "\tTEST1: file that doesn't exist" << clr_non << '\n';
@@ -84,10 +90,14 @@ int	main(
 	ret_val = funny_sed(filename, str1, str2);
 	if (ret_val == 0)
 		cout << '\n';
-	cout << clr_yel << "RETURN VALUE: " << clr_cya << ret_val << "\033[0m\n\n";
+	cout << clr_yel << "RETURN VALUE: " << clr_cya << ret_val << "\033[0m\n";
+	print_file_contents(filename);
+	print_file_contents(filename_r);
+	cout << '\n';
 
 	// TEST 2: unmodifable file
 	filename = "testfiles/noaccess.txt";
+	filename_r = "testfiles/noaccess.txt.replace";
 	str1 = "can't";
 	str2 = "can";
 	cout << clr_yel << "\tTEST2: file with no read perm" << clr_non << '\n';
@@ -95,21 +105,29 @@ int	main(
 	ret_val = funny_sed(filename, str1, str2);
 	if (ret_val == 0)
 		cout << '\n';
-	cout << clr_yel << "RETURN VALUE: " << clr_cya << ret_val << "\033[0m\n\n";
+	cout << clr_yel << "RETURN VALUE: " << clr_cya << ret_val << "\033[0m\n";
+	print_file_contents(filename);
+	print_file_contents(filename_r);
+	cout << '\n';
 
 	// TEST 3: .replace file already exists
 	filename = "testfiles/imreal";
-	str1 = "can't";
-	str2 = "can";
+	filename_r = "testfiles/imreal.replace";
+	str1 = "meow";
+	str2 = "bark";
+	std::fstream	original_file("testfiles/imreal");
+	original_file << "this meow will be replaced with bark";
 	cout << clr_yel << "\tTEST3: .replace file exists and can be modded" << clr_non << '\n';
 	cout << clr_yel << "OUTPUT: " << clr_non;
 	ret_val = funny_sed(filename, str1, str2);
 	if (ret_val == 0)
 		cout << '\n';
 	cout << clr_yel << "RETURN VALUE: " << clr_cya << ret_val << "\033[0m\n";
+	std::ifstream file2("testfiles/imreal.replace");
+	print_file_contents(filename);
+	print_file_contents(filename_r);
 }
 //tests to write:
-//3. file.replace already exists and is modifiable;
 //4. file.replace already exists and not modifiable;
 //5. really big file;
 //6. empty file;
